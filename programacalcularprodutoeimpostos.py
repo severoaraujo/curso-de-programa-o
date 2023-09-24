abfe8375d76ce8e601f37cc80ccb9ffdcebfd949
Thalles Severo <145393863+severoaraujo@users.noreply.github.com>
2023-09-24 20:29:47 -0300

    Update programacalcularprodutoeimpostos.py

--- programacalcularprodutoeimpostos.py
+++ programacalcularprodutoeimpostos.py
@@ -9,15 +9,15 @@ def calcular_preco_venda(preco_custo):
     return preco_venda
 
 # Solicitar entrada de dados do usuário
-valor_produto = float(input("100"))
-quantidade = int(input("100"))
-imposto1 = float(input("60"))
-imposto2 = float(input("150"))
-imposto3 = float(input("100 "))
+valor_produto = float(input("650,"60","60","100","50","30","60","120","150","50")
+quantidade = int(input(("bicicleta","cadeado","chave","ferramentas","pneu","camera","capacete","roupa","sapato","oculos": "))
+imposto1 = float(input("60","30","40","50","30","15","20","50","60"))
+imposto2 = float(input("50","40","40","50","35","20","25","45","55"))
+imposto3 = float(input("55","25","35","45","30","25","20","40",50"))
 valor_frete = float(input("50"))
 
 # Calcular o preço de custo
-preco_custo = calcular_preco_custo(100, 100, 60, 150, 100, 50)
+preco_custo = calcular_preco_custo(100)
 
 # Calcular o preço de venda
 preco_venda = calcular_preco_venda(50)
