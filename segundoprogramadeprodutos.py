ed9f7849fdf8f82f246a43ae2de6029ae1699710
Thalles Severo <145393863+severoaraujo@users.noreply.github.com>
2023-09-26 23:26:39 -0300

    Update segundoprogramadeprodutos.py

--- segundoprogramadeprodutos.py
+++ segundoprogramadeprodutos.py
@@ -1,59 +1,116 @@
-# Lista para armazenar os detalhes dos produtos
+ Inicialize uma lista vazia para armazenar os detalhes dos produtos
 produtos = []
 
-calcular_preco_custo(valor_compra, quantidade, frete_total):
-    preco_custo = 1000 + (100 / 100)
+def calcular_preco_custo(valor_compra, frete_total, quantidade_compra, imposto1, imposto2, imposto3):
+    frete_por_unidade = frete_total / quantidade_compra
+    preco_custo = valor_compra + frete_por_unidade
+    preco_custo += preco_custo * (imposto1 + imposto2 + imposto3)
     return preco_custo
 
-calcular_preco_venda(preco_custo, porcentagem_lucro, imposto1, imposto2, imposto3):
-    preco_venda = preco_custo * (1 + (porcentagem_lucro / 100))
-    preco_venda += preco_venda * (imposto1 / 100)
-    preco_venda += preco_venda * (imposto2 / 100)
-    preco_venda += preco_venda * (imposto3 / 100)
-    return preco_venda
+def calcular_preco_venda(preco_custo):
+    return preco_custo * 1.6  # 60% de lucro
+imposto1 = 0.12
+imposto2 = 0.06
+imposto3 = 0.03
 
 while True:
-    print("\nOpções:")
-    print("1. Inserir informações de um novo produto")
-    print("2. Imprimir detalhes do produto atual")
-    print("3. Sair do programa")
+    print("\n===== Menu =====")
+    print("1. Inserir informações do produto")
+    print("2. Atualizar informações do produto")
+    print("3. Deletar produto")
+    print("4. Imprimir detalhes do produto atual")
+    print("5. Sair")
     
-    escolha = input("Escolha uma opção: ")
+    opcao = input("Escolha uma opção (1/2/3/4/5): ")
     
-    if escolha == "1":
-        nome_produto = input(cafe,açucar,arroz,feijao)
-        valor_compra = float(input("15 ",10,20,6))
-        quantidade = int(input("10,20,30,40: "))
-        frete_total = float(input("50"))
-        porcentagem_lucro = float(input("100%"))
-        imposto1 = float(input("0.12"))
-        imposto2 = float(input("0.10 "))
-        imposto3 = float(input("0.15"))
-
-        preco_custo = calcular_preco_custo(valor_compra, quantidade, frete_total)
-        preco_venda = calcular_preco_venda(preco_custo, porcentagem_lucro, imposto1, imposto2, imposto3)
-
-        produto = {
-            "Nome": nome_produto,
+    if opcao == '1':
+        # Solicitar informações sobre o produto
+        nome_produto = input("Nome do produto: ")
+        valor_compra = float(input("Valor da compra: "))
+        quantidade_compra = int(input("Quantidade de compra: "))
+        frete_total = float(input("Frete total da quantidade comprada: "))
+        
+        # Calcular o preço de custo
+        preco_custo = calcular_preco_custo(valor_compra, frete_total, quantidade_compra, imposto1, imposto2, imposto3)
+        
+        # Calcular o preço de venda com base na porcentagem de lucro
+        preco_venda = calcular_preco_venda(preco_custo)
+        
+        # Criar um dicionário com os detalhes do produto
+        detalhes_produto = {
+            "Nome do Produto": nome_produto,
+            "Valor da Compra": valor_compra,
+            "Quantidade de Compra": quantidade_compra,
+            "Frete Total": frete_total,
             "Preço de Custo": preco_custo,
-            "Preço de Venda": preco_venda,
-            "Quantidade": quantidade
+            "Preço de Venda": preco_venda
         }
         
-        produtos.append(produto)
-        print("Informações do produto foram adicionadas com sucesso!")
-
-    elif escolha == "2":
-        if not produtos:
-            print("Nenhum produto cadastrado ainda.")
+        # Adicionar os detalhes do produto à lista de produtos
+        produtos.append(detalhes_produto)
+        print("Detalhes do produto foram salvos com sucesso.")
+    
+    elif opcao == '2':
+        # Atualizar informações do produto
+        if len(produtos) == 0:
+            print("Nenhum produto foi inserido ainda.")
         else:
-            print("\nDetalhes do produto atual:")
-            for chave, valor in produtos[-1].items():
+            print("Lista de produtos:")
+            for i, produto in enumerate(produtos):
+                print(f"{i + 1}. {produto['Nome do Produto']}")
+            
+            escolha = int(input("Escolha o número do produto que deseja atualizar: "))
+            if 1 <= escolha <= len(produtos):
+                produto_atualizar = produtos[escolha - 1]
+                
+                # Solicitar informações atualizadas
+                valor_compra = float(input("Novo valor da compra: "))
+                quantidade_compra = int(input("Nova quantidade de compra: "))
+                frete_total = float(input("Novo frete total da quantidade comprada: "))
+                
+                # Atualizar os detalhes do produto
+                produto_atualizar["Valor da Compra"] = valor_compra
+                produto_atualizar["Quantidade de Compra"] = quantidade_compra
+                produto_atualizar["Frete Total"] = frete_total
+                
+                # Recalcular preço de custo e preço de venda
+                produto_atualizar["Preço de Custo"] = calcular_preco_custo(valor_compra, frete_total, quantidade_compra, imposto1, imposto2, imposto3)
+                produto_atualizar["Preço de Venda"] = calcular_preco_venda(produto_atualizar["Preço de Custo"])
+                
+                print("Detalhes do produto atualizados com sucesso.")
+            else:
+                print("Escolha inválida.")
+    
+    elif opcao == '3':
+        # Deletar produto
+        if len(produtos) == 0:
+            print("Nenhum produto foi inserido ainda.")
+        else:
+            print("Lista de produtos:")
+            for i, produto in enumerate(produtos):
+                print(f"{i + 1}. {produto['Nome do Produto']}")
+            
+            escolha = int(input("Escolha o número do produto que deseja deletar: "))
+            if 1 <= escolha <= len(produtos):
+                produto_deletar = produtos.pop(escolha - 1)
+                print(f"O produto '{produto_deletar['Nome do Produto']}' foi deletado com sucesso.")
+            else:
+                print("Escolha inválida.")
+    
+    elif opcao == '4':
+        # Imprimir detalhes do produto atual
+        if len(produtos) == 0:
+            print("Nenhum produto foi inserido ainda.")
+        else:
+            produto_atual = produtos[-1]
+            print("\n===== Detalhes do Produto Atual =====")
+            for chave, valor in produto_atual.items():
                 print(f"{chave}: {valor}")
-
-    elif escolha == "3":
+    
+    elif opcao == '5':
+        # Sair do programa
         print("Saindo do programa.")
         break
-
+    
     else:
-        print("Opção inválida. Por favor, escolha uma opção válida.")
+        print("Opção inválida. Por favor, escolha uma opção válida (1/2/3/4/5).")
